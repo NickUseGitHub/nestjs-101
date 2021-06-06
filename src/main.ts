d81de0d88c4113e67f7878a76578a6933c356db2
Chaiwtun <chaiwtun@scg.com>
2021-06-06 23:34:14 +0700

    [master] apply global prefix

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  app.setGlobalPrefix('api/v1');
   await app.listen(configService.get<number>('PORT'));
 }
 bootstrap();
